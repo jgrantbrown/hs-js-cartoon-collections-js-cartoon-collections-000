e28445061625169b2318eab05756c73e1e7a6299
jason brown <j.grant.brown@gmail.com>
2016-12-12 05:24:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function dwarfRollCall(dwarves) {
   var theList=("");
   for (var i=0; dwarves.length>i;i++){
-  theList+=((i+1)+"."+array[i]+" ");
-  return theList.slice(0,-1);
-}}
+  theList+=((i+1)+ ". " + dwarves[i]+" ")};
+  return theList;
+}
 
 function summonCaptainPlanet(planeteerCalls){
-  var newCall =[];
-  for(var i=0;planeteerCalls.length>i;i++){
-    newCall+=(planteerCAlls[i]+"!");}
-    return newCall.toUpperCase();
-  }
+var newCall=[];
+for(var i=0; planeteerCalls.length>i;i++){
+newCall+=(planeteerCalls[i] + "!");}
+return newCall.toUpperCase();
+}
 
 
 function longPlaneteerCalls(words) {
-  for (var i=0;i<arr.length;i++){
-    if(arr[i].length<=4)
+  for (var i=0;i<words.length;i++){
+    if(words[i].length<=4)
     return false;
     else{
       return true
