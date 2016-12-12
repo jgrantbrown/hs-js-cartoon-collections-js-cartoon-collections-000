c48dc25e41e90b92c54bc2cdaad01e52dc45d1bf
jason brown <j.grant.brown@gmail.com>
2016-12-12 05:10:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function dwarfRollCall(dwarves) {
   var theList=("");
-  for (var i=0; dwarves.length>i;i++)
+  for (var i=0; dwarves.length>i;i++){
   theList+=(i+1)+"."+array[i]+" ")
   return theList.slice(0,-1)
-}
+}}
 
 function summonCaptainPlanet(planeteerCalls){
   var newCall =[];
