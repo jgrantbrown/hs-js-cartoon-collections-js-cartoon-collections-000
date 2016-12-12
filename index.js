09df9d3771c76a50f18783bb061e4869735f1697
jason brown <j.grant.brown@gmail.com>
2016-12-12 05:50:12 +0000

    Done.

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  for(i=0;i<foods.length;i++){
+  for(var i=0;i<foods.length;i++){
     if(foods[i]==="cheddar"|| "gouda"|| "camembert")
       return arr[i]
       else{
         return "no cheese"
       }
-    }
   }
 }
