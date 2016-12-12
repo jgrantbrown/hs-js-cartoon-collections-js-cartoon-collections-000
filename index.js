2bfec73c11d7ce5635a8d5a451a22e9b5168809e
jason brown <j.grant.brown@gmail.com>
2016-12-12 05:08:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function dwarfRollCall(dwarves) {
+  var theList=("");
+  for (var i=0; dwarves.length>i;i++)
+  theList+=(i+1)+"."+array[i]+" ")
+  return theList.slice(0,-1)
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newCall =[];
+  for9var i=0;planeteerCalls.length>i;i++){
+    newCall+=(planteerCAlls[i]+"!");}
+    return newCall.toUpperCase();
+  }
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0;i<arr.length;i++){
+    if(arr[i].length<=4)
+    return false;
+    else{
+      return true
+    }
+  }
 }
 
 function findTheCheese (foods) {
